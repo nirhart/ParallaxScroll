dd53d1f9d18d097e53e8c2270fa4e77ad439c7a4
Pol Gómez Guerrero <thebestpol@gmail.com>
2017-10-29 08:57:37 +0100

    Updated build tools  (#63)
    * Updating gradle version
    
    * Updated buildtools and pushed version name and code
    
    * Solved huge mistake, PR that doesn't compile

--- ParallaxScroll/src/com/nirhart/parallaxscroll/views/ParallaxedView.java
+++ ParallaxScroll/src/com/nirhart/parallaxscroll/views/ParallaxedView.java
@@ -1,49 +1,51 @@
 package com.nirhart.parallaxscroll.views;
 
-import java.lang.ref.WeakReference;
-import java.util.ArrayList;
-import java.util.List;
-
 import android.annotation.SuppressLint;
+import android.graphics.Rect;
 import android.os.Build;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
 
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class ParallaxedView {
-	static public boolean isAPI11 = Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB;
-	static public boolean isAPI18 = Build.VERSION.SDK_INT >= 18;
-	protected WeakReference<View> view;
-	protected int lastOffset;
-	protected List<Animation> animations;
-	protected Rect clippingRect = new Rect();
+    private static boolean isAPI11 = Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB;
+    private static boolean isAPI18 = Build.VERSION.SDK_INT >= 18;
+    protected WeakReference<View> view;
+    protected int lastOffset;
+    protected List<Animation> animations;
+    protected Rect clippingRect = new Rect();
     protected boolean firstRectGet = false;
+    private int lastScrollY;
 
-	abstract protected void translatePreICS(View view, float offset);
-	
-	public ParallaxedView(View view) {
-		this.lastOffset = 0;
-		this.animations = new ArrayList<Animation>();
-		this.view = new WeakReference<View>(view);
-	}
+    abstract protected void translatePreICS(View view, float offset);
 
-	public boolean is(View v) {
-		return (v != null && view != null && view.get() != null && view.get().equals(v));
-	}
+    public ParallaxedView(View view) {
+        this.lastOffset = 0;
+        this.animations = new ArrayList<Animation>();
+        this.view = new WeakReference<View>(view);
+    }
 
-	@SuppressLint("NewApi")
-	public void setOffset(float offset) {
-		View view = this.view.get();
-		if (view != null)
-			if (isAPI11) {
-				view.setTranslationY(offset);
-			} else {
-				translatePreICS(view, offset);
-			}
-	}
+    public boolean is(View v) {
+        return (v != null && view != null && view.get() != null && view.get().equals(v));
+    }
 
-	@SuppressLint("NewApi")
+    @SuppressLint("NewApi")
+    public void setOffset(float offset) {
+        View view = this.view.get();
+        if (view != null)
+            if (isAPI11) {
+                view.setTranslationY(offset);
+            } else {
+                translatePreICS(view, offset);
+            }
+    }
+
+    @SuppressLint("NewApi")
     public void setOffset(float offset, int originalScrollY) {
         View view = this.view.get();
         if (view != null)
@@ -54,6 +56,9 @@ public abstract class ParallaxedView {
                     clippingRect.right = view.getRight();
                     clippingRect.bottom = view.getBottom();
                     firstRectGet = true;
+                }
+                if (lastScrollY == 0) {
+                    lastScrollY = originalScrollY;
                 }
                 int delta = lastScrollY - originalScrollY;
                 clippingRect.bottom += delta;
@@ -72,40 +77,40 @@ public abstract class ParallaxedView {
             }
     }
 
-	public void setAlpha(float alpha) {
-		View view = this.view.get();
-		if (view != null)
-			if (isAPI11) {
-				view.setAlpha(alpha);
-			} else {
-				alphaPreICS(view, alpha);
-			}
-	}
-	
-	protected synchronized void addAnimation(Animation animation) {
-		animations.add(animation);
-	}
-	
-	protected void alphaPreICS(View view, float alpha) {
-		addAnimation(new AlphaAnimation(alpha, alpha));
-	}
-	
-	protected synchronized void animateNow() {
-		View view = this.view.get();
-		if (view != null) {
-			AnimationSet set = new AnimationSet(true);
-			for (Animation animation : animations)
-				if (animation != null)
-					set.addAnimation(animation);
-			set.setDuration(0);
-			set.setFillAfter(true);
-			view.setAnimation(set);
-			set.start();
-			animations.clear();
-		}
-	}
-	
-	public void setView(View view) {
-		this.view = new WeakReference<View>(view);
-	}
+    public void setAlpha(float alpha) {
+        View view = this.view.get();
+        if (view != null)
+            if (isAPI11) {
+                view.setAlpha(alpha);
+            } else {
+                alphaPreICS(view, alpha);
+            }
+    }
+
+    protected synchronized void addAnimation(Animation animation) {
+        animations.add(animation);
+    }
+
+    protected void alphaPreICS(View view, float alpha) {
+        addAnimation(new AlphaAnimation(alpha, alpha));
+    }
+
+    protected synchronized void animateNow() {
+        View view = this.view.get();
+        if (view != null) {
+            AnimationSet set = new AnimationSet(true);
+            for (Animation animation : animations)
+                if (animation != null)
+                    set.addAnimation(animation);
+            set.setDuration(0);
+            set.setFillAfter(true);
+            view.setAnimation(set);
+            set.start();
+            animations.clear();
+        }
+    }
+
+    public void setView(View view) {
+        this.view = new WeakReference<View>(view);
+    }
 }
